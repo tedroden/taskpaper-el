4b0e444064dada936878991576ae3c97585dc94c
Ted Roden <tedroden@gmail.com>
2010-02-25 13:42:22 -0500

    Updates:  - changed some of the faces  - updated the font-lock-keywords for @done  - taskpaper-create-project now adds "- " (took out the support for "+" as a task)
    Additions:
     - added new font-lock-keywords for tasks tagged with @priority([1-3])
     - added new font-lock-keywords for tasks tagged with @today
     - Turn off "font-lock-string-face" for the whole mode
     - added taskpaper-newline-and-electric-mark which allows you to create a new task on the next line from anywhere in the previous line (handy when editing a task)
     - added the ability to decrease and increase the priority of a task with taskpaper-priority-increase, taskpaper-priority-decrease, and taskpaper-priority-adjust

--- taskpaper.el
+++ taskpaper.el
@@ -1,8 +1,10 @@
 ;;; taskpaper.el --- Taskpaper implementation for Emacs
 
 ;; Copyright (C) 2008 Kentaro Kuribayashi
+;; Copyright (C) 2010 Ted Roden
 
 ;; Author: kentaro <kentarok@gmail.com>
+;; Author: Ted Roden <tedroden@gmail.com>
 ;; Keywords: tools, task
 
 ;; Modified Feb 2010 by Jonas Oberschweiber <jonas@oberschweiber.com>
@@ -88,56 +90,104 @@
 (define-key taskpaper-mode-map "\C-c\C-d" 'taskpaper-toggle-task)
 (define-key taskpaper-mode-map "+"        'taskpaper-electric-mark)
 (define-key taskpaper-mode-map "-"        'taskpaper-electric-mark)
+(define-key taskpaper-mode-map (kbd "M-RET") 'taskpaper-newline-and-electric-mark)
+(define-key taskpaper-mode-map (kbd "M-<up>") 'taskpaper-priority-increase)
+(define-key taskpaper-mode-map (kbd "M-<down>") 'taskpaper-priority-decrease)
+
 
 ;; Face
 (defface taskpaper-project-face
   '((((class color) (background light))
-     (:foreground "white" :underline "red" :weight bold))
+     (:foreground "white" :underline "darkred" :weight bold :family "8x13" :height 2.0))
     (((class color) (background dark))
-     (:foreground "white" :underline "red" :weight bold)))
+     (:foreground "white" :underline "darkred" :weight bold :family "8x13" :height 2.0)))
   "Face definition for project name")
 
 (defface taskpaper-task-face
   '((((class color) (background light))
-     (:foreground "yellow"))
+     (:foreground "wheat4"))
     (((class color) (background dark))
-     (:foreground "yellow")))
+     (:foreground "wheat4")))
   "Face definition for task")
 
 (defface taskpaper-task-marked-as-done-face
   '((((class color) (background light))
-     (:foreground "dim grey" :weight light :strike-through t))
+     (:foreground "grey12" :weight light :strike-through t))
     (((class color) (background dark))
-     (:foreground "dim grey" :weight light :strike-through t)))
+     (:foreground "grey12" :weight light :strike-through t)))
   "Face definition for task marked as done")
 
 (defface taskpaper-done-mark-face
   '((((class color) (background light))
-     (:foreground "dim grey"))
+     (:foreground "grey12"))
     (((class color) (background dark))
-     (:foreground "dim grey")))
+     (:foreground "grey12")))
   "Face definition for done mark")
 
 (defface taskpaper-undone-mark-face
   '((((class color) (background light))
-     (:foreground "blue"))
+     (:foreground "yellow"))
     (((class color) (background dark))
-     (:foreground "blue")))
+     (:foreground "yelow")))
   "Face definition for undone mark")
+
+(defface taskpaper-task-priority-3-face
+  '((((class color) (background light))
+     (:foreground "red1"))
+    (((class color) (background dark))
+     (:foreground "red1")))
+  "Priority 3 Face")
+
+(defface taskpaper-task-priority-2-face
+  '((((class color) (background light))
+     (:foreground "OrangeRed1"))
+    (((class color) (background dark))
+     (:foreground "OrangeRed1")))
+  "Priority 2 Face")
+
+(defface taskpaper-task-priority-1-face
+  '((((class color) (background light))
+     (:foreground "orange1"))
+    (((class color) (background dark))
+     (:foreground "orange1")))
+  "Priority 1 Face")
+
+(defface taskpaper-task-today-face
+  '((((class color) (background light))
+     (:foreground "LimeGreen"))
+    (((class color) (background dark))
+     (:foreground "LimeGreen")))
+  "today's tasks Face")
+
 
 (defvar taskpaper-project-face 'taskpaper-project-face)
 (defvar taskpaper-task-face 'taskpaper-task-face)
 (defvar taskpaper-task-marked-as-done-face 'taskpaper-task-marked-as-done-face)
 (defvar taskpaper-done-mark-face 'taskpaper-done-mark-face)
 (defvar taskpaper-undone-mark-face 'taskpaper-undone-mark-face)
+(defvar taskpaper-task-today-face 'taskpaper-task-today-face)
+(defvar taskpaper-task-priority-1-face 'taskpaper-task-priority-1-face)
+(defvar taskpaper-task-priority-2-face 'taskpaper-task-priority-2-face)
+(defvar taskpaper-task-priority-3-face 'taskpaper-task-priority-3-face)
+
 (defvar taskpaper-font-lock-keywords
-  '(("^.+:[ \t]*$" 0 taskpaper-project-face)
-    ("^[ \t]*\\(-\\)\\(.*\\)\\@done\\(.*\\)$"
-     (1 taskpaper-done-mark-face t)
-     (2 taskpaper-task-marked-as-done-face))
-    ("^[ \t]*\\(-\\)\\(.*\\)$"
+  '(
+	("^.+:[ \t]*$" 0 taskpaper-project-face)
+    ("^[ \t]*\\(-\\)\\(.*\\).*$"
      (1 taskpaper-undone-mark-face t)
-     (2 taskpaper-task-face))))
+     (2 taskpaper-task-face t))
+
+    ("^[ \t]*\\(-\\)\\(.+\\)@done.*$"
+     (0 taskpaper-done-mark-face t)
+     (2 taskpaper-task-marked-as-done-face t))
+
+
+	(".+@today.*" 0 taskpaper-task-today-face t)
+
+	("^.+@priority\(1\)$" 0 taskpaper-task-priority-1-face t)
+	("^.+@priority\(2\)$" 0 taskpaper-task-priority-2-face t)
+	("^.+@priority\(3\)$" 0 taskpaper-task-priority-3-face t)))
+
 
 ;; Taskpaper major mode
 (define-derived-mode taskpaper-mode fundamental-mode "Taskpaper"
@@ -148,10 +198,14 @@
   (setq mode-name "Taskpaper")
   (use-local-map taskpaper-mode-map)
   (set (make-local-variable 'font-lock-defaults) '(taskpaper-font-lock-keywords))
+  (set (make-local-variable 'font-lock-string-face) nil)
   (set (make-local-variable 'indent-line-function) 'taskpaper-indent-line)
   (run-hooks 'taskpaper-mode-hook))
 
 (add-to-list 'auto-mode-alist (cons "\\.taskpaper$" 'taskpaper-mode))
+;;(setq       font-lock-string-face nil)
+
+
 
 ;; Commands
 (defun taskpaper-create-new-project (name)
@@ -162,17 +216,16 @@
 (defun taskpaper-create-new-task (task)
   "Creates new task"
   (interactive "sNew Task: ")
-  (insert (concat "+ " task))
-  (newline-and-indent))
+  (insert (concat "- " task)))
 
-;; I don't use it, so I didn't touch it.
+
 (defun taskpaper-toggle-task ()
   "Marks task as done"
   (interactive)
   (save-excursion
     (beginning-of-line)
-    (when (looking-at "[-+]")
-      (let ((mark (if (equal (match-string 0) "+") "-" "+")))
+    (when (looking-at "[- ]")
+      (let ((mark (if (equal (match-string 0) " ") "-" " ")))
         (delete-char 1)
         (insert mark)))))
 
@@ -188,7 +241,7 @@
       (while (and (not in-project) (not (bobp)))
         (forward-line -1)
         (when (looking-at "^.+:[ \t]*$") (setq in-project t))
-        (when (looking-at "[+-]") (setq mark-flag t))))
+        (when (looking-at "-") (setq mark-flag t))))
     (when mark-flag (insert "- "))    
     (when in-project (indent-line-to taskpaper-indent-amount))
     (setq indent-tabs-mode old-tabs)))
@@ -202,6 +255,71 @@
         (self-insert-command arg)      
         (insert " "))
     (self-insert-command arg)))
+
+
+(defun taskpaper-newline-and-electric-mark ()
+  "Newline and new task"
+  (interactive)
+  (progn
+	(end-of-line) 
+	(insert "\n")
+	(taskpaper-indent-line)
+	(insert "- ")))
+
+
+
+(defun taskpaper-priority-increase ()
+  "increase the priority by one"
+  (interactive)
+  (taskpaper-priority-adjust 1))
+
+(defun taskpaper-priority-decrease ()
+  "increase the priority by one"
+  (interactive)
+  (taskpaper-priority-adjust -1))
+
+(defun taskpaper-priority-adjust (number)
+  "Adjust the priority by x"
+  (interactive)
+  (save-excursion
+	(progn
+	  ;; get to the start of the line
+	  (beginning-of-line)
+
+	  ;; is there a priority already defined
+	  (if (looking-at ".*\\(@priority(\\([0-9]\\))\\).*")
+
+
+		  ;; cache the current-priority
+		  (let ((current-priority (string-to-number 
+								   (buffer-substring-no-properties 
+									(match-beginning 2) 
+									(match-end 2)))))
+
+			(setq new-priority (+ number current-priority))
+
+			;; if the priority goes to zero, remove it all together
+			(if (> 1 new-priority)
+				(delete-region (- 1 (match-beginning 1)) (match-end 1))
+
+			  ;; otherwise, delete the current-priority
+			  (delete-region (match-beginning 2) (match-end 2))
+
+			  ;; go to the inside of the priority() parens
+			  (goto-char (match-beginning 2))
+
+			  ;; insert the updated priority
+			  (insert (number-to-string new-priority))))
+
+		;; otherwise, if we're increasing append a basic priority
+		(message (number-to-string number))
+		(if (< number 0)
+			(message "No priority to decrease")
+		  
+		  (end-of-line)
+		  (insert " @priority(1)"))))))
+
+
 
 (provide 'taskpaper)
 ;;; taskpaper.el ends here
